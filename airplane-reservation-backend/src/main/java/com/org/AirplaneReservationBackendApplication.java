906111386c607b2101fc8bb00437dfea07e74c77
Pavan Lalwani <pavan_lalwani@persistent.com>
2021-09-20 17:05:46 +0530

    Backend base files

---
+++ airplane-reservation-backend/src/main/java/com/org/AirplaneReservationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirplaneReservationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirplaneReservationBackendApplication.class, args);
+	}
+
+}
