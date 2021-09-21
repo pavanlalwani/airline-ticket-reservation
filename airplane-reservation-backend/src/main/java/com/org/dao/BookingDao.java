a8e8c74fc05cb9cfc66a03d27b65f6e44b068851
Suhadini Kaluarachchi <kasuhadini@gmail.com>
2021-09-21 13:05:35 +0530

    Resolved an issue in BookingDao

--- airplane-reservation-backend/src/main/java/com/org/dao/BookingDao.java
+++ airplane-reservation-backend/src/main/java/com/org/dao/BookingDao.java
@@ -5,7 +5,7 @@ import java.math.BigInteger;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.org.model.Booking;
+import com.org.models.Booking;
 
 @Repository
 public interface BookingDao extends CrudRepository<Booking, BigInteger> {
