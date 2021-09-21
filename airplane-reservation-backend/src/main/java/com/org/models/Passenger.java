4cf3ea11b469ed7ca2ef9fc18f69ab91ab9c0b20
r-nandan-kumar <91062006+r-nandan-kumar@users.noreply.github.com>
2021-09-21 12:42:22 +0530

    Added Passenger model

---
+++ airplane-reservation-backend/src/main/java/com/org/models/Passenger.java
@@ -0,0 +1,59 @@
+package com.org.models;
+
+import java.math.BigInteger;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Passenger {
+	@Id
+	private BigInteger passengerNumber;
+	private String passengerName;
+	private int passengerAge;
+	private BigInteger passengerUIN;
+	private Double luggage;
+
+	public BigInteger getPassengerNumber() {
+		return passengerNumber;
+	}
+
+	public void setPassengerNumber(BigInteger passengerNumber) {
+		this.passengerNumber = passengerNumber;
+	}
+
+	public String getPassengerName() {
+		return passengerName;
+	}
+
+	public void setPassengerName(String passengerName) {
+		this.passengerName = passengerName;
+	}
+
+	public int getPassengerAge() {
+		return passengerAge;
+	}
+
+	public void setPassengerAge(int passengerAge) {
+		this.passengerAge = passengerAge;
+	}
+
+	public BigInteger getPassengerUIN() {
+		return passengerUIN;
+	}
+
+	public void setPassengerUIN(BigInteger passengerUIN) {
+		this.passengerUIN = passengerUIN;
+	}
+
+	public Double getLuggage() {
+		return luggage;
+	}
+
+	public void setLuggage(Double luggage) {
+		this.luggage = luggage;
+	}
+
+}
+
+
+}
