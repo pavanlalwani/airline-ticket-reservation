93354be82590b69ff04d745808bd433f1ebe7428
r-nandan-kumar <91062006+r-nandan-kumar@users.noreply.github.com>
2021-09-29 17:24:19 +0530

    Update AddFlightDetailsTest.java

--- airplane-reservation-backend/src/test/java/com/org/AddFlightDetailsTest.java
+++ airplane-reservation-backend/src/test/java/com/org/AddFlightDetailsTest.java
@@ -13,7 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import com.org.dao.FlightDao;
-import com.org.models.Flight;
+import com.org.model.Flight;
 import com.org.service.FlightService;
 
 @SpringBootTest
